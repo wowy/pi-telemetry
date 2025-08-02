4d50cb21bbc288cff089968bd1066eb50b59d2d9
Rory Lonergan <github@deadbear.racing>
2025-08-02 09:20:37 -0500

    Add code to parse the coolant temp message

--- receive.py
+++ receive.py
@@ -1,15 +1,38 @@
 import os
 import can
+import time
 
 os.system("sudo ip link set can0 type can bitrate 100000")
 os.system("sudo ifconfig can0 up")
 
 can0 = can.interface.Bus(channel="can0", bustype="socketcan")  # socketcan_native
 
-# msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
-msg = can0.recv(10.0)
-print(msg)
-if msg is None:
-    print("Timeout occurred, no message.")
+# Set up a filter for messages with ID 0x3E0
+can0.set_filters([{"can_id": 0x3E0, "can_mask": 0x7FF, "extended": False}])
 
-os.system("sudo ifconfig can0 down")
+try:
+    print("Waiting for CAN message with ID 0x3E0...")
+    while True:
+        msg = can0.recv(10.0)
+        if msg is None:
+            print("Timeout occurred, no message.")
+            continue
+            
+        if msg.arbitration_id == 0x3E0:
+            print(f"Received message: {msg}")
+            
+            # Extract bits 0-1 from the first byte of data
+            # This gets the 2 least significant bits (0 and 1)
+            value = msg.data[0] & 0b00000011
+            
+            # Divide by 10
+            result = value / 10.0
+            
+            print(f"Extracted value (bits 0-1): {value}")
+            print(f"Result after division by 10: {result}")
+            break
+            
+except KeyboardInterrupt:
+    print("Program terminated by user")
+finally:
+    os.system("sudo ifconfig can0 down")
