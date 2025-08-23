74498462ad57d4dc927b6c775b8b4ef18295bb37
Rory Lonergan <github@deadbear.racing>
2025-08-23 08:48:47 -0500

    Uncomment setup lines.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#sudo ip link set can0 type can bitrate 100000
-#sudo ifconfig can0 txqueuelen 65536
-#sudo ifconfig can0 up
+sudo ip link set can0 type can bitrate 100000
+sudo ifconfig can0 txqueuelen 65536
+sudo ifconfig can0 up
 
 python3 src/pi-telemetry/receive.py
