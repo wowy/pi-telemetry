56b7f149d386fb12639428f3133d541c9b282f1b
Rory Lonergan <github@deadbear.racing>
2026-03-17 22:45:54 -0500

    Update Swift argument parser.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "pi-telemetry",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
